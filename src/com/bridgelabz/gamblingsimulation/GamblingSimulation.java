575104ee0da9e04b12d0d4c56f8f7f6270406d9e
prem-12345 <premkpatil1997@gmail.com>
2021-08-14 07:26:00 +0530

    WELCOME

---
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gamblingsimulation;
+
+public class GamblingSimulation {
+
+    //WELCOME TO GAMBLING SIMULATION PROGRAM
+
+    public static void main(String[] args) {
+
+    }
+}
