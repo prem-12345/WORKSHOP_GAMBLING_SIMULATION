1946f0f559560609541c6838f5219ef329a1bdc6
prem-12345 <premkpatil1997@gmail.com>
2021-08-14 07:36:26 +0530

    TAKING_STAKE_AS_100_AND_BET_AS_1

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -4,6 +4,15 @@ public class GamblingSimulation {
 
     //WELCOME TO GAMBLING SIMULATION PROGRAM
 
+    public static void stake(){
+
+        //UC1 TAKING STAKE AS 100 AND BET AS 1
+
+        public static final int STAKE = 100;
+        public static final int BET = 1;
+
+    }
+
     public static void main(String[] args) {
 
     }
