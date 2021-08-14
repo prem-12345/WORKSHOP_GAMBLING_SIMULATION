c70091d880a0a3c175bad1a298222ed99ff26fc2
prem-12345 <premkpatil1997@gmail.com>
2021-08-14 07:49:28 +0530

    CHECK_WIN_OR_LOSS

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulation.java
@@ -1,19 +1,32 @@
 package com.bridgelabz.gamblingsimulation;
 
+import java.util.Random;
+
 public class GamblingSimulation {
 
     //WELCOME TO GAMBLING SIMULATION PROGRAM
 
-    public static void stake(){
+    //UC1 TAKING STAKE AS 100 AND BET AS 1
 
-        //UC1 TAKING STAKE AS 100 AND BET AS 1
+    public static final int STAKE = 100;
+    public static final int BET = 1;
 
-        public static final int STAKE = 100;
-        public static final int BET = 1;
+    public static void winOrLoss() {
 
+        //UC2 CHECK WIN OR LOSS
+
+        Random random = new Random();
+        int Num = random.nextInt(2);
+        if (Num == 1) {
+            System.out.println(" GAMBLER WIN ");
+
+        } else {
+            System.out.println(" GAMBLER LOSS ");
+
+        }
     }
 
     public static void main(String[] args) {
-
+        GamblingSimulation.winOrLoss();
     }
 }
